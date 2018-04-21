d0805dc0a513e5ebc29deaa3459cc4e89c320c66
Marvin Dawson <marvintdawson@gmail.com>
2018-04-20 19:34:32 -0500

    updated profile with site-data provider

--- src/pages/project-list/project-list.ts
+++ src/pages/project-list/project-list.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {ProjectsListService} from "../../services/project-list";
+import { SiteDataProvider } from "../../providers/site-data/site-data";
 
 @IonicPage()
 @Component({
   selector: 'page-project-list',
   templateUrl: 'project-list.html',
 })
+
 export class ProjectsListPage {
 
   constructor(private projlistServic: ProjectsListService,
               public navCtrl: NavController,
-              public navParams: NavParams) {
-  }
+              public navParams: NavParams,
+              private siteData: SiteDataProvider) {}
 
-
+              siteName = this.siteData.siteName;
 
 }
