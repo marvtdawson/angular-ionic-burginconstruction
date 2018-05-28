199a598b78b189988b943bbd3fdbdd8bb6f6ab2a
Marvin Dawson <marvintdawson@gmail.com>
2018-05-28 08:16:11 -0500

    Deleted app-firebase-config.ts also reset and rm cached

--- src/app/app-firebase-config.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAMjgdggf9heGV-OBbZrjP-XYTe3WU4vZQ",
-  authDomain: "burginquickquoter.firebaseapp.com",
-  databaseURL: "https://burginquickquoter.firebaseio.com",
-  projectId: "burginquickquoter",
-  storageBucket: "burginquickquoter.appspot.com",
-  messagingSenderId: "755170744178"
-}
